3daf0dd06671579bf022ff4ecb12fed1831b2c64
Julian Grinblat <julian@dotcore.co.il>
2022-04-30 22:46:51 +0900

    Remove dependency on ascallback

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,7 @@
-import pkg from './package.json';
-
 const input = 'src/index.js';
-const external = Object.keys(pkg.dependencies);
 
 export default [{
   input,
-  external,
   // sourcemaps help generate coverage reports for the actual sources using istanbul
   output: {
     file: 'dist/index.cjs',
@@ -15,7 +11,6 @@ export default [{
   },
 }, {
   input,
-  external,
   output: {
     file: 'dist/index.js',
     format: 'esm',
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import asCallback from 'ascallback';
-
-const defaultHandleError = (val) => Promise.reject(val);
-
-export default ({ handleError = defaultHandleError } = {}) => (socket, next) => {
+export default ({ handleError = Promise.reject.bind(Promise) } = {}) => (socket, next) => {
   const on = socket.on.bind(socket);
   socket.on = (event, handler, ...args) => { // eslint-disable-line no-param-reassign
     const newHandler = (...handlerArgs) => {
       const cb = handlerArgs.at(-1);
-      const res = handler(...handlerArgs)?.catch?.((err) => handleError(err, event));
 
-      if (typeof res?.then === 'function') {
-        asCallback(res, cb);
-      }
+      handler(...handlerArgs)
+        ?.catch?.((err) => handleError(err, event))
+        ?.then(cb.bind(null, null), cb);
     };
 
     on(event, newHandler, ...args);
